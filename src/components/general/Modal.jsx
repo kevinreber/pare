fd843a3a6adae2ca02847e78a2bde8a859ed53ee
kevinreber <kevinreber1@gmail.com>
2020-08-12 17:56:04 -0700

    added style to Modal component

--- src/components/general/Modal.jsx
+++ src/components/general/Modal.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { ReactComponent as CloseIcon } from './icons/close-icon.svg';
+import './styles/Modal.css';
 
 /** Boilerplate template for any actions that require a Modal */
-function Modal({content, closeModal }){
-    return(
-        <div className="Modal">
-            <div className="Modal-Close float-left">
-                <h4 onClick={closeModal}><CloseIcon /></h4>
-            </div>
-            <div className="Modal-Content">
-                { content }
-            </div>
-        </div>
-    )
+function Modal({ content, closeModal }) {
+	return (
+		<div className='Modal'>
+			<div className='Modal-Close float-left'>
+				<h4 onClick={closeModal}>
+					<CloseIcon />
+				</h4>
+			</div>
+			<div className='Modal-Content'>{content}</div>
+		</div>
+	);
 }
 
 export default Modal;
