6102f682c1fcb30e3f84d8bae7bef39ac47fc263
kevinreber <kevinreber1@gmail.com>
2020-08-15 13:32:00 -0700

    style: restyled ClassList to remove <a> in <table> error

--- src/components/courses/Course.jsx
+++ src/components/courses/Course.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 function Course({ course }) {
 	return (
 		<>
-			<table className='mate-table table-hover'>
+			{/* <table className='mate-table table-hover'>
 				<tbody className='Course Course-Card'>
 					<tr>
 						<Link to={`/courses/${course.id}`} className='mate-text-secondary'>
@@ -21,7 +21,22 @@ function Course({ course }) {
 						</Link>
 					</tr>
 				</tbody>
-			</table>
+			</table> */}
+			<Link to={`/courses/${course.id}`} className='mate-text-secondary'>
+				<table className='mate-table table-hover'>
+					<tbody className='Course Course-Card'>
+						<tr>
+							<td className='mate-text-primary Course-Name'>
+								{`${course.department} ${course.number}`} <br />
+								<span className='mate-text-secondary Course-Card-Term pt-1 pb-2'>
+									{course.term}
+								</span>
+							</td>
+							<td className='pl-3 Course-Title'>{course.name}</td>
+						</tr>
+					</tbody>
+				</table>
+			</Link>
 		</>
 	);
 }
