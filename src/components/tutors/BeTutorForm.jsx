922ac60fa228467f95bbd0056a484459da8211de
kevinreber <kevinreber1@gmail.com>
2020-08-25 12:02:25 -0700

    wip: be tutor form

--- src/components/tutors/BeTutorForm.jsx
+++ src/components/tutors/BeTutorForm.jsx
@@ -1,30 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
 import SubmitButton from '../general/SubmitButton';
 import useFields from '../../hooks/useFields';
+import { TextField } from '@material-ui/core';
 
-function BeTutorForm({ availability, save }) {
+function BeTutorForm({ user, save }) {
 	// Form Data
 	const INITIAL_STATE = {
 		keywords: '',
 		portfolio: '',
-		availability: '',
+		monday: new Date(),
+		tuesday: '',
+		wednesday: '',
+		thursday: '',
+		friday: '',
+		saturday: '',
+		sunday: '',
 	};
 
-	const [formData, handleChange, resetFormData] = useFields(INITIAL_STATE);
+	// const [formData, handleChange, resetFormData] = useFields(INITIAL_STATE);
+
+	const [formData, setFormData] = useState(INITIAL_STATE);
+
+	/** Handles general fields in form */
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormData((fData) => ({ ...fData, [name]: value }));
+	};
+
+	/** Handles 'dueDate' field */
+	const handleDate = (e, date) => {
+		const { name } = e.target;
+		setFormData((fData) => ({ ...fData, [name]: date }));
+	};
+
+	/** Handles 'dueDate' field */
+	// const handleMon = (date) => {
+	// 	setFormData((fData) => ({ ...fData, monday: date }));
+	// };
+
+	const convert = (name, value) => ({
+		target: {
+			name,
+			value,
+		},
+	});
+
+	const handleMon = (e) => {
+		const { name, value } = e.target;
+		console.log(name, value);
+		setFormData((fData) => ({ ...fData, [name]: value }));
+	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		save(formData);
 
 		// Clear state of form
-		resetFormData();
+		setFormData(INITIAL_STATE);
 	};
 
 	return (
 		<div className='BeTutorForm'>
 			<form className='container mb-3' onSubmit={handleSubmit}>
 				<div className='form-group'>
-					<label htmlFor='courseMajor' className='float-left'>
+					<label htmlFor='keywords' className='float-left'>
 						I can help in...
 					</label>
 					<input
@@ -48,21 +88,115 @@ function BeTutorForm({ availability, save }) {
 						name='portfolio'
 						value={formData.portfolio}
 					/>
+					<i class='fas fa-plus-circle'></i>
 				</div>
+				<DatePicker
+					name='monday'
+					selected={formData.monday}
+					onChange={handleMon}
+					showTimeSelect
+					showTimeSelectOnly
+					timeIntervals={30}
+					timeCaption='Time'
+					dateFormat='h:mm aa'
+				/>
+				<TextField
+					name='monday'
+					value={formData.monday}
+					onChange={(date) => handleMon(convert('monday', date))}
+					id='time'
+					// label=''
+					type='time'
+					// defaultValue={formData.monday}
+					// className={}
+					InputLabelProps={{
+						shrink: true,
+					}}
+					inputProps={{
+						step: 300, // 5 min
+					}}
+				/>
 				<div className='form-group'>
-					<label htmlFor='availability' className='float-left'>
-						Availability
+					{/* <label className='float-left'>Availability</label> */}
+					<label htmlFor='monday' className='float-left'>
+						Monday
 					</label>
 					<input
-						id='availability'
+						id='monday'
 						className='form-control mate-form-input'
 						type='text'
 						onChange={handleChange}
-						name='availability'
-						value={formData.availability}
+						name='monday'
+						value={formData.monday}
+					/>
+					<label htmlFor='tuesday' className='float-left'>
+						Tuesday
+					</label>
+					<input
+						id='tuesday'
+						className='form-control mate-form-input'
+						type='text'
+						onChange={handleChange}
+						name='tuesday'
+						value={formData.tuesday}
+					/>{' '}
+					<label htmlFor='wednesday' className='float-left'>
+						Wednesday
+					</label>
+					<input
+						id='wednesday'
+						className='form-control mate-form-input'
+						type='text'
+						onChange={handleChange}
+						name='wednesday'
+						value={formData.wednesday}
+					/>
+					<label htmlFor='thursday' className='float-left'>
+						Thursday
+					</label>
+					<input
+						id='thursday'
+						className='form-control mate-form-input'
+						type='text'
+						onChange={handleChange}
+						name='thursday'
+						value={formData.thursday}
+					/>
+					<label htmlFor='friday' className='float-left'>
+						Friday
+					</label>
+					<input
+						id='friday'
+						className='form-control mate-form-input'
+						type='text'
+						onChange={handleChange}
+						name='friday'
+						value={formData.friday}
+					/>
+					<label htmlFor='saturday' className='float-left'>
+						Saturday
+					</label>
+					<input
+						id='saturday'
+						className='form-control mate-form-input'
+						type='text'
+						onChange={handleChange}
+						name='saturday'
+						value={formData.saturday}
+					/>
+					<label htmlFor='sunday' className='float-left'>
+						Sunday
+					</label>
+					<input
+						id='sunday'
+						className='form-control mate-form-input'
+						type='text'
+						onChange={handleChange}
+						name='sunday'
+						value={formData.sunday}
 					/>
 				</div>
-				<SubmitButton text='Join' />
+				<SubmitButton text='Save' />
 			</form>
 		</div>
 	);
