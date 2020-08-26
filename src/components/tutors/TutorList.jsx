16accc087b7a1c8ebb873ff778e6ed07a9ce0277
kevinreber <kevinreber1@gmail.com>
2020-08-25 23:18:16 -0700

    added default value for tutors

--- src/components/tutors/TutorList.jsx
+++ src/components/tutors/TutorList.jsx
@@ -1,31 +1,7 @@
 import React from 'react';
 import TutorCard from './TutorCard';
 
-const tutorsDemo = [
-	{
-		id: 1,
-		name: 'John',
-		avatar: 'https://randomuser.me/api/portraits/thumb/men/75.jpg',
-		tutorSubjects: 'Computer Science',
-		classes: 'Math1A, CS61A, CS61B',
-	},
-	{
-		id: 2,
-		name: 'Angela',
-		avatar: 'https://randomuser.me/api/portraits/thumb/men/75.jpg',
-		tutorSubjects: 'Computer Science',
-		classes: 'Math1A, CS61A, CS61B',
-	},
-	{
-		id: 3,
-		name: 'Tony',
-		avatar: 'https://randomuser.me/api/portraits/thumb/men/75.jpg',
-		tutorSubjects: 'Math',
-		classes: 'Math1A, CS61A, CS61B',
-	},
-];
-
-function TutorList({ tutors = tutorsDemo }) {
+function TutorList({ tutors = [] }) {
 	const List = tutors.map((tutor) => (
 		<TutorCard
 			id={tutor.id}
