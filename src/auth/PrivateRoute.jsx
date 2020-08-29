2e683a3c8d57a0fc113141150a0ebda9485f30b4
kevinreber <kevinreber1@gmail.com>
2020-08-28 17:18:59 -0700

    fixed user store name

--- src/auth/PrivateRoute.jsx
+++ src/auth/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 /** Will only allow access to certain routes based on User's credentials */
 function PrivateRoute({ path, component }) {
 	/** User must be logged in, in order to access Private Routes */
-	const currentUser = useSelector((state) => state.auth.auth);
+	const currentUser = useSelector((state) => state.auth.user);
 
 	/** if user isn't logged in, redirect to login page */
 	if (!currentUser) {
