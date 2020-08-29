14f535421df40796f80a1f6cea5a33c3db7972ef
kevinreber <kevinreber1@gmail.com>
2020-08-29 09:07:55 -0700

    switched icon with material UI icon

--- src/components/general/Modal.jsx
+++ src/components/general/Modal.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { ReactComponent as CloseIcon } from './icons/close-icon.svg';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { IconButton } from '@material-ui/core';
 import './styles/Modal.css';
 
 /** Boilerplate template for any actions that require a Modal */
@@ -7,9 +9,12 @@ function Modal({ content, closeModal }) {
 	return (
 		<div className='Modal'>
 			<div className='Modal-Close float-left'>
-				<h4 onClick={closeModal}>
-					<CloseIcon />
-				</h4>
+				{/* <h4 onClick={closeModal}> */}
+				<IconButton onClick={closeModal}>
+					<ArrowBackIosIcon />
+				</IconButton>
+				{/* <CloseIcon /> */}
+				{/* </h4> */}
 			</div>
 			<div className='Modal-Content'>{content}</div>
 		</div>
