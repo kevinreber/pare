b9ceb142322d69e4898dcec910110115c330fad5
kevinreber <kevinreber1@gmail.com>
2020-09-03 23:29:44 -0700

    added "Reset" button to clear form data to SubmitButtons

--- src/components/general/SubmitButton.jsx
+++ src/components/general/SubmitButton.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
 /** Submit Button for forms */
-function SubmitButton({ text }) {
+function SubmitButton({ text, reset = false, resetForm }) {
 	return (
 		<div className='container w-75'>
 			<button className='btn mate-btn mate-btn-primary mate-btn-submit'>
 				{text}
 			</button>
+			{reset ? (
+				<>
+					<button
+						type='button'
+						onClick={resetForm}
+						className='btn mate-btn mate-btn-secondary mate-btn-reset'>
+						Clear
+					</button>
+				</>
+			) : (
+				''
+			)}
 		</div>
 	);
 }
