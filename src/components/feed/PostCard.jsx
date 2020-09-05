973984c725e1f4f78928c17d26e3742ffccca1fe
kevinreber <kevinreber1@gmail.com>
2020-09-04 20:06:03 -0700

    re-styled Feeds page

--- src/components/feed/PostCard.jsx
+++ src/components/feed/PostCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 import IconButton from '@material-ui/core/IconButton';
 import ModeCommentOutlinedIcon from '@material-ui/icons/ModeCommentOutlined';
 import CalendarTodayOutlinedIcon from '@material-ui/icons/CalendarTodayOutlined';
@@ -11,14 +12,20 @@ import Avatar from '@material-ui/core/Avatar';
 
 function PostCard({
 	id,
+	key,
 	title,
 	username,
-	user_id,
+	userId,
 	avatar,
-	content,
+	description,
 	location,
-	comments,
+	type,
+	start,
+	end,
+	attachment_preview,
+	attachment,
 	timestamp,
+	comments,
 	isBookmarked = false,
 }) {
 	const BookmarkStatus = !isBookmarked ? (
@@ -27,42 +34,69 @@ function PostCard({
 		<BookmarkIcon />
 	);
 
+	const convertedTimestamp = moment(timestamp.toDate())
+		.startOf('day')
+		.fromNow();
+
+	/** converts time */
+	const convertTime = (time) => {
+		// const converted = new Date(time?.toDate()).toUTCString();
+		return moment(time.toDate()).calendar();
+	};
+
+	const eventTime =
+		start && end ? (
+			<>
+				<p className='event__time'>
+					Start: <span className='event__timestamp'> {convertTime(start)}</span>
+				</p>
+				<p className='event__time'>
+					Ends: <span className='event__timestamp'> {convertTime(end)}</span>
+				</p>
+			</>
+		) : (
+			''
+		);
+
 	return (
-		<div className='Post-Card Mate-Card'>
-			<div className='Post-Card__Left'>
-				<Link to={`/users/${user_id}`} className='mate-text-secondary'>
-					<Avatar alt={username} src={avatar} />
-				</Link>
-			</div>
-			<div className='Post-Card__Center'>
-				<Link to={`/post/${id}`}>
-					<div className='Post-Card__Header'>
-						<h5>{title}</h5>
-						<span className='mate-text-secondary'>{username}</span>
-					</div>
-					<div className='Post-Card__Body mate-text-secondary'>
-						<p>{content}</p>
-						<span>{location}</span>
-					</div>
-				</Link>
-				<div className='Post-Card__Footer'>
-					<IconButton>
-						<ModeCommentOutlinedIcon />
-					</IconButton>
-					<IconButton>
-						<CalendarTodayOutlinedIcon />
-					</IconButton>
-					<IconButton>{BookmarkStatus}</IconButton>
-					<IconButton>
-						<SendIcon />
-					</IconButton>
-					<IconButton>
-						<ShareIcon />
-					</IconButton>
+		<div key={key} className='Post-Card'>
+			<div className='Post-Card__Main'>
+				<div className='Post-Card__Left'>
+					<Link to={`/users/${userId}`} className='mate-text-secondary'>
+						<Avatar alt={username} src={avatar} />
+					</Link>
+				</div>
+				<div className='Post-Card__Center'>
+					<Link to={`/post/${id}`}>
+						<div className='Post-Card__Header'>
+							<h5>{title}</h5>
+							<span className='mate-text-secondary username'>{username}</span>
+						</div>
+						<div className='Post-Card__Body mate-text-secondary'>
+							<p>{description}</p>
+							<span className='location'>{location}</span>
+							<span>{eventTime}</span>
+						</div>
+					</Link>
 				</div>
 			</div>
-			<div className='Post-Card__Right'>
-				<p>3hrs</p>
+			<div className='Post-Card__Footer'>
+				<IconButton>
+					<ModeCommentOutlinedIcon />
+				</IconButton>
+				<IconButton>
+					<CalendarTodayOutlinedIcon />
+				</IconButton>
+				<IconButton>{BookmarkStatus}</IconButton>
+				<IconButton>
+					<SendIcon />
+				</IconButton>
+				<IconButton>
+					<ShareIcon />
+				</IconButton>
+			</div>
+			<div className='Post-Card__Right Post__timestamp'>
+				<p>{convertedTimestamp}</p>
 			</div>
 		</div>
 	);
