0063f4f44164dff7d9e4a81894e0009bd9130c7b
kevinreber <kevinreber1@gmail.com>
2020-09-11 10:54:20 -0700

    added filter query for Study Groups

--- src/components/general/NoData.jsx
+++ src/components/general/NoData.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 /** Returns a message if user has not added any data */
-function NoData({ text }) {
-	return <p className='font-italic p-3'>No {text} added</p>;
+function NoData({ text, added = true }) {
+	return (
+		<p className='font-italic p-3'>
+			No {text} {added ? 'added' : ''}
+		</p>
+	);
 }
 
 export default NoData;
--- src/pages/StudyGroups.jsx
+++ src/pages/StudyGroups.jsx
@@ -3,14 +3,16 @@ import React, { useState, useEffect } from 'react';
 
 /** Components & Helpers */
 import CTAButton from '../components/general/CTAButton';
-import Autocomplete from '../components/general/Autocomplete';
+import Searchbar from '../components/general/Searchbar';
 import NoData from '../components/general/NoData';
 import StudyGroupList from '../components/StudyGroups/StudyGroupList';
 import db from '../config/fbConfig';
 import './styles/StudyGroups.css';
 
+/** Page that displays a list of user's Study Groups */
 function Connect() {
 	const [studyGroups, setStudyGroups] = useState([]);
+	const [filter, setFilter] = useState('');
 
 	useEffect(() => {
 		db.collection('study-group').onSnapshot((snapshot) =>
@@ -23,18 +25,49 @@ function Connect() {
 		);
 	}, []);
 
-	const List =
-		studyGroups && studyGroups.length !== 0 ? (
-			<StudyGroupList studyGroups={studyGroups} />
-		) : (
-			<NoData text={'study groups'} />
+	let List;
+
+	if (filter !== '' && studyGroups && studyGroups.length !== 0) {
+		const filteredGroups = studyGroups.filter(
+			(studyGroup) =>
+				studyGroup.data.title.toLowerCase().indexOf(filter.toLowerCase()) > -1
 		);
+
+		List =
+			filteredGroups.length > 0 ? (
+				<StudyGroupList
+					studyGroups={
+						filteredGroups // filter study groups to display
+					}
+				/>
+			) : (
+				<NoData text={'matches'} added={false} />
+			);
+	} else if (studyGroups && studyGroups.length !== 0) {
+		List = <StudyGroupList studyGroups={studyGroups} />;
+	} else {
+		List = <NoData text={'study groups'} />;
+	}
+	// let List =
+	// 	filter !== '' && studyGroups && studyGroups.length !== 0 ? (
+	// 		// filter study groups to display
+	// 		<StudyGroupList
+	// 			studyGroups={studyGroups.filter(
+	// 				(studyGroup) =>
+	// 					studyGroup.data.title.toLowerCase().indexOf(filter.toLowerCase()) > -1
+	// 			)}
+	// 		/>
+	// 	) : studyGroups && studyGroups.length !== 0 ? (
+	// 		<StudyGroupList studyGroups={studyGroups} />
+	// 	) : (
+	// 		<NoData text={'study groups'} />
+	// 	);
 
 	return (
 		<div className='StudyGroups'>
 			<div className='Body-Header StudyGroups__Header'>
 				<h5>My Study Groups</h5>
-				<Autocomplete />
+				<Searchbar value={filter} setValue={setFilter} />
 			</div>
 			<div className='StudyGroups__Body'>{List}</div>
 			<div className='CourseForm p-3'>
