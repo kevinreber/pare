73ffa439e5b8ac32f2251e95fbca547bb9cadb6a
kevinreber <kevinreber1@gmail.com>
2020-09-12 17:31:44 -0700

    comments

--- src/store/actions/types.js
+++ src/store/actions/types.js
@@ -1,7 +1,10 @@
+/** Admin */
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT_USER = 'LOGOUT_USER';
 export const LOGOUT_FAIL = 'LOGOUT_FAIL';
+
+/** Feed */
 export const ADD_POST = 'ADD_POST';
 export const ADD_POST_FAIL = 'ADD_POST_FAIL';
 export const EDIT_POST = 'EDIT_POST';
@@ -14,16 +17,26 @@ export const EDIT_COMMENT_ON_POST = 'EDIT_COMMENT_ON_POST';
 export const EDIT_COMMENT_ON_POST_FAIL = 'EDIT_COMMENT_ON_POST_FAIL';
 export const REMOVE_COMMENT_ON_POST = 'REMOVE_COMMENT_ON_POST';
 export const REMOVE_COMMENT_ON_POST_FAIL = 'REMOVE_COMMENT_ON_POST_FAIL';
+
+/** Course Catalog */
+export const FETCH_CATALOG = 'FETCH_CATALOG';
+export const FETCH_CATALOG_ERROR = 'FETCH_CATALOG_ERROR';
+
+/** Courses */
 export const FETCH_COURSES = 'FETCH_COURSES';
 export const FETCH_COURSE = 'FETCH_COURSE';
 export const ADD_COURSE = 'ADD_COURSE';
 export const REMOVE_COURSE = 'REMOVE_COURSE';
+
+/** Courses Assignments */
 export const FETCH_ASSIGNMENTS = 'FETCH_ASSIGNMENTS';
 export const ADD_ASSIGNMENT = 'ADD_ASSIGNMENT';
-export const FETCH_CATALOG = 'FETCH_CATALOG';
-export const FETCH_CATALOG_ERROR = 'FETCH_CATALOG_ERROR';
+
+/** Search */
 export const QUICK_SEARCH_BY_TODAY = 'QUICK_SEARCH_BY_TODAY';
 export const QUICK_SEARCH_BY_TYPE = 'QUICK_SEARCH_BY_TYPE';
+
+/** Tutor - User Availability */
 export const UPDATE_AVAILABILITY = 'UPDATE_AVAILABILITY';
 export const ADD_AVAILABILITY = 'ADD_AVAILABILITY';
 export const REMOVE_AVAILABILITY = 'REMOVE_AVAILABILITY';
