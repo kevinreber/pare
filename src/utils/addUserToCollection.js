062a5ea646f00c98a90a8c8783d963943bccc484
kevinreber <kevinreber1@gmail.com>
2020-10-05 19:01:17 -0700

    updates new DB schema

--- src/utils/addUserToCollection.js
+++ src/utils/addUserToCollection.js
@@ -11,8 +11,24 @@ async function addUserToCollection(collection, docId, field, data) {
 
 	// Add data to collection group's array of users
 	if (data) {
-		ref.update({ [field]: firebase.firestore.FieldValue.arrayUnion(data) });
-		console.log('success');
+		if (collection === 'study-group') {
+			console.log(collection, docId, field, data);
+			// ref.update({ [field]: data });
+			db.collection(collection)
+				.doc(docId)
+				.collection('users')
+				.doc(field)
+				.set(data);
+
+			// Append users ID to usersList
+			await ref.update({
+				usersList: firebase.firestore.FieldValue.arrayUnion(data.uid),
+			});
+			console.log('success');
+		} else {
+			ref.update({ [field]: firebase.firestore.FieldValue.arrayUnion(data) });
+			console.log('success');
+		}
 	}
 }
 
