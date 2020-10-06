7d0462752fa07d3effef42263d31316c44908168
kevinreber <kevinreber1@gmail.com>
2020-10-05 19:48:50 -0700

    added user subcollection to new study-group collections

--- src/utils/createNewMessage.js
+++ src/utils/createNewMessage.js
@@ -4,14 +4,18 @@ import db from '../config/fbConfig';
 /**
  * Creates a new message in the DB and returns the newMessage.id
  *
+ * @param {String} collection
  * @param {object} messageData
+ * @param {String} subCollection
  * @param {object} chatData
+ * @param {object} user
  */
 async function createNewMessage(
 	collection,
 	messageData,
 	subCollection = null,
-	chatData = null
+	chatData = null,
+	user = null
 ) {
 	// Make new document
 	const newMessage = await db.collection(collection).add(messageData);
@@ -22,6 +26,20 @@ async function createNewMessage(
 			.doc(newMessage.id)
 			.collection(subCollection)
 			.add(chatData);
+
+		// if 'study-group' collection, add user to 'users' subcollection
+		if (collection === 'study-group') {
+			db.collection(collection)
+				.doc(newMessage.id)
+				.collection('users')
+				.doc(user.uid)
+				.set({
+					uid: user.uid,
+					admin: true,
+					displayName: user.displayName,
+					photoURL: user.photoURL,
+				});
+		}
 	}
 
 	return newMessage.id;
