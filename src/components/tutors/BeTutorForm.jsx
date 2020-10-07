b0ec9257ce7ac96922a098de100e92fb3ecc8712
kevinreber <kevinreber1@gmail.com>
2020-10-06 18:08:06 -0700

    added portfolio URL verification

--- src/components/tutors/BeTutorForm.jsx
+++ src/components/tutors/BeTutorForm.jsx
@@ -1,5 +1,6 @@
 /** Dependencies */
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import firebase from 'firebase';
 import moment from 'moment';
 import { original, produce } from 'immer';
@@ -9,6 +10,7 @@ import DatePicker from 'react-datepicker';
 import CTAButton from '../general/CTAButton';
 import useFields from '../../hooks/useFields';
 import createFbTimestamp from '../../utils/createFbTimestamp';
+import { addFlashMessage } from '../../store/actions/flashMessages';
 import db from '../../config/fbConfig';
 
 /** MUI */
@@ -29,6 +31,8 @@ const DAYS = [
 ];
 
 function BeTutorForm({ uid, user, update, availability }) {
+	const dispatch = useDispatch();
+
 	// Form Data
 	const INITIAL_STATE = {
 		keywords: '',
@@ -38,6 +42,7 @@ function BeTutorForm({ uid, user, update, availability }) {
 	console.log(user);
 	const [formData, setFormData] = useState(INITIAL_STATE);
 	const [changeMade, setChangeMade] = useState(false);
+
 	// const availability = {
 	// 	monday: {
 	// 		start: moment(new Date()).format(),
@@ -61,6 +66,7 @@ function BeTutorForm({ uid, user, update, availability }) {
 		}
 	};
 
+	// if no changes made to date, save button will not highlight
 	if (changeMade && formData.keywords === '' && formData.portfolio === '') {
 		setChangeMade(false);
 	}
@@ -142,15 +148,67 @@ function BeTutorForm({ uid, user, update, availability }) {
 		</div>
 	));
 
+	// Returns true if data is verified
+	const verifyData = (data, variant) => {
+		// Verifies if data is empty string
+		if (variant === 'keywords') {
+			if (data === '' || data.trim() === '') {
+				dispatch(
+					addFlashMessage({
+						isOpen: true,
+						message: 'Field can not be empty',
+						type: 'error',
+					})
+				);
+				return false;
+			}
+		}
+		// Verifies if data is valid URL
+		if (variant === 'portfolio') {
+			const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+			if (!regexp.test(data)) {
+				dispatch(
+					addFlashMessage({
+						isOpen: true,
+						message: 'Invalid URL',
+						type: 'error',
+					})
+				);
+				return false;
+			}
+		}
+		return true;
+	};
+
+	// Adds user field data from DB
+	const addData = (field, data) => {
+		if (verifyData(data, field)) {
+			db.collection('users')
+				.doc(uid)
+				.update({
+					[field]: firebase.firestore.FieldValue.arrayUnion(data),
+				});
+			updateUser(uid);
+			// reset field data
+			setFormData((fData) => ({
+				...fData,
+				[field]: '',
+			}));
+		}
+	};
+
 	// Removes user field data from DB
 	const removeData = (field, data) => {
-		console.log(field, data);
 		db.collection('users')
 			.doc(uid)
 			.update({
 				[field]: firebase.firestore.FieldValue.arrayRemove(data),
 			});
+		updateUser(uid);
+	};
 
+	// updates user's last update timestamp
+	const updateUser = (uid) => {
 		db.collection('users').doc(uid).update({
 			lastUpdatedAt: createFbTimestamp(),
 		});
@@ -191,7 +249,7 @@ function BeTutorForm({ uid, user, update, availability }) {
 					<label className="float-left">Portfolio Links</label>
 					<input
 						className="form-control mate-form-input"
-						type="url"
+						type="text"
 						onChange={handleChange}
 						name="portfolio"
 						value={formData.portfolio}
@@ -203,7 +261,8 @@ function BeTutorForm({ uid, user, update, availability }) {
 						</ul>
 					) : null}
 					<div className="Portfolio__Add">
-						<IconButton>
+						<IconButton
+							onClick={() => addData('portfolio', formData.portfolio)}>
 							<AddCircleOutlineRoundedIcon />
 						</IconButton>
 					</div>
