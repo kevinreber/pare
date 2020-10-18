798fa622d1fe636e47ae39f6d2981c0980d2b289
kevinreber <kevinreber1@gmail.com>
2020-10-17 21:23:36 -0700

    styled tutor list elements

--- src/components/tutors/TutorList.jsx
+++ src/components/tutors/TutorList.jsx
@@ -2,11 +2,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+/** Components & Helpers */
+import NoData from '../general/NoData';
+
+/** MUI */
+import Chip from '@material-ui/core/Chip';
+
 function TutorList({ tutors = [] }) {
+
+	// Build list of elements for User Keywords and User Classes
+	const fieldList = (field, arrayOfData) => {
+		return arrayOfData.map((data, index) => (
+			<li data-name={field} key={index}>
+				<Chip
+					label={data}
+					size='small'
+				/>
+			</li>
+		));
+	};
+
+	// Build list of tutors
 	const List = tutors.map((tutor) => (
 		<>
-			<Link to={`/users/${tutor.id}`}>
-				<div key={tutor.id} className="mate-table table-hover">
+			<li key={tutor.id} className="mate-table table-hover">
+				<Link to={`/users/${tutor.id}`}>
 					<div className="Tutor Tutor-Card">
 						<div className="mr-3">
 							<img
@@ -15,22 +35,47 @@ function TutorList({ tutors = [] }) {
 								className="Tutor Tutor-Avatar Avatar"
 							/>
 						</div>
-						<div className="mate-text-primary text-left mr-auto">
-							{tutor.data.displayName} <br />
-							<span className="mate-text-secondary Tutor-Card-Term">
-								{/* {tutor.classes} */}
-							</span>
-						</div>
-						<div className="Tutor-Title mate-text-secondary text-left">
-							{/* {tutor.tutorSubjects} */}
+						<div className="Tutor-Card-Content mate-text-primary text-left mr-auto">
+							<div className="Tutor-Card__Header">
+								{tutor.data.displayName} <br />
+							</div>
+							<div className="Tutor-Card__Body">
+								<div className="Tutor-Card__Classes">
+									{tutor.data.classes.length > 0 ? 
+									(<>
+										<p>Classes Taken: </p>
+										<ul className='User-Classes'>
+											{fieldList('classes', tutor.data.classes)}
+										</ul>
+									</>) : null}
+								</div>
+								<div className="Tutor-Card__Keywords">
+									{tutor.data.keywords.length > 0 ? 
+									(<>
+										<p>I can help in: </p>
+										<ul className='User-Keywords'>
+											{fieldList('keywords', tutor.data.keywords)}
+										</ul>
+									</>) : null}
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
-			</Link>
+				</Link>
+			</li>
 		</>
 	));
 
-	return <>{List}</>;
+	return (
+		<>
+			{tutors.length > 0 ?
+				<ul className='Tutor-List'>
+					{List}
+				</ul> 
+				: ( <NoData text="tutors available" added={false}/>)
+			}
+		</>
+	)
 }
 
 export default TutorList;
