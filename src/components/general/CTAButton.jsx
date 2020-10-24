516b6f557ff9b21ad6d99dfee123394d1d04caca
kevinreber <kevinreber1@gmail.com>
2020-10-24 10:49:20 -0700

    added comments

--- src/components/general/CTAButton.jsx
+++ src/components/general/CTAButton.jsx
@@ -1,9 +1,14 @@
+/** Dependencies */
 import React from 'react';
+import { PropTypes } from 'prop-types';
 
-/** Call To Action Button */
+/** Call To Action Button
+ * @param {string}	 text	 Text to display inside CTA Button.
+ * @param {boolean}	 danger	 Boolean to change color of CTA Button.
+ */
 function CTAButton({ text, danger = false }) {
 	return (
-		<div className='container w-75'>
+		<div className="container w-75">
 			<button
 				className={`btn mate-btn  mate-btn-cta ${
 					danger ? 'mate-btn-danger' : 'mate-btn-secondary'
@@ -13,5 +18,10 @@ function CTAButton({ text, danger = false }) {
 		</div>
 	);
 }
+
+CTAButton.propTypes = {
+	text: PropTypes.string,
+	danger: PropTypes.bool,
+};
 
 export default CTAButton;
