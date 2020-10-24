f5cd4a048ccb2cce0924a8c62f4868985cb561f5
kevinreber <kevinreber1@gmail.com>
2020-10-24 10:46:42 -0700

    setup FindTutors form

--- src/components/tutors/FindTutors.jsx
+++ src/components/tutors/FindTutors.jsx
@@ -4,11 +4,13 @@ import { PropTypes } from 'prop-types';
 
 /** Components & Helpers */
 import TutorList from './TutorList';
+import CTAButton from '../general/CTAButton';
 import capitalizeFirstLetter from '../../utils/capitalizeFirstLetter';
 
 /** MUI */
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import SearchIcon from '@material-ui/icons/Search';
 
 /** Days of the week */
 const DAYS = [
@@ -40,6 +42,7 @@ function FindTutors({ tutors, isLoading }) {
 
 	const [filteredTutors, setFilteredTutors] = useState([]);
 	const [availability, setAvailability] = useState(AVAILABILITY_INITIAL_STATE);
+	const [search, setSearch] = useState('');
 
 	const handleChange = (e) => {
 		const { name, value } = e.target;
@@ -48,23 +51,46 @@ function FindTutors({ tutors, isLoading }) {
 	};
 
 	return (
-		<>
-			<input type="text" />
-			{DAYS.map((day, index) => (
-				<FormControlLabel
-					key={index + 1}
-					control={
-						<Checkbox
-							checked={availability[day]}
-							onChange={handleChange}
-							name={day}
+		<div className="Find-Tutors">
+			<form>
+				<div className="form-group">
+					<label htmlFor="tutor-search">I need help in...</label>
+					<div className="Search__Elements">
+						<div className="Search__Icon">
+							<SearchIcon />
+						</div>
+						<input
+							id="tutor-search"
+							className="form-control mate-form-input"
+							type="search"
+							onChange={(e) => setSearch(e.target.value)}
+							name="search"
+							value={search}
+							maxLength="30"
+							placeholder="eg. photoshop, python, math"
 						/>
-					}
-					label={capitalizeFirstLetter(day)}
-				/>
-			))}
+					</div>
+				</div>
+				<div className="form-group Find-Availability">
+					<label>Availability</label>
+					{DAYS.map((day, index) => (
+						<FormControlLabel
+							key={index + 1}
+							control={
+								<Checkbox
+									checked={availability[day]}
+									onChange={handleChange}
+									name={day}
+								/>
+							}
+							label={capitalizeFirstLetter(day)}
+						/>
+					))}
+				</div>
+				<CTAButton text="Search" />
+			</form>
 			<TutorList tutors={filteredTutors} isLoading={isLoading} />
-		</>
+		</div>
 	);
 }
 
