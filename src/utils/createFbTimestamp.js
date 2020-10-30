247cff8e3525cab5280d3fc6776e4a19a9a4740d
kevinreber <kevinreber1@gmail.com>
2020-10-29 22:42:43 -0700

    added comments

--- src/utils/createFbTimestamp.js
+++ src/utils/createFbTimestamp.js
@@ -1,6 +1,17 @@
 /** Dependencies */
 import firebase from 'firebase';
 
+/** Returns firebase timestamp
+ * @returns	{object}	Object of firebase timestamp that has seconds and nanoseconds keys.
+ *
+ * ex:
+ * 	timestamp = { seconds: ..., nanoseconds: ... };
+ *
+ * Has built in methods for easier reading timestamps.
+ *   ex: timestamp.toDate(), timestamp.compareTo(timestamp2), timestamp.getSeconds()
+ *
+ * To learn more about firebase timestamp methods, look into Firebase Timestamp.
+ */
 function createFbTimestamp() {
 	return firebase.firestore.FieldValue.serverTimestamp();
 }
--- src/utils/createNewMessage.js
+++ src/utils/createNewMessage.js
@@ -1,15 +1,17 @@
+/** Dependencies */
+import { PropTypes } from 'prop-types';
+
 /** Components & Helpers */
 import addUserToCollection from './addUserToCollection';
 import db from '../config/fbConfig';
 
-/**
- * Creates a new message in the DB and returns the newMessage.id
+/** Creates a new message in the DB and returns the newMessage.id
  *
- * @param {String} collection
- * @param {object} messageData
- * @param {String} subCollection
- * @param {object} chatData
- * @param {object} user
+ * @param 	{String} 	collection			Collection to be accessed.
+ * @param 	{object} 	messageData			Message Data being 'sent'.
+ * @param 	{String} 	subCollection		Sub-collection to be accessed.
+ * @param 	{object} 	chatData			Chat Data to be added to 'chats' sub-collection.
+ * @param 	{object} 	user				Object of user data.
  */
 async function createNewMessage(
 	collection,
@@ -21,7 +23,7 @@ async function createNewMessage(
 	// Make new document
 	const newMessage = await db.collection(collection).add(messageData);
 
-	// if 'study-group' collection, add user to 'users' subcollection
+	// if 'study-group' collection, add user to 'users' sub-collection
 	if (collection === 'study-group') {
 		db.collection(collection).doc(newMessage.id).set(messageData);
 
@@ -44,5 +46,13 @@ async function createNewMessage(
 
 	return newMessage.id;
 }
+
+createNewMessage.propTypes = {
+	collection: PropTypes.string.isRequired,
+	messageData: PropTypes.object.isRequired,
+	subCollection: PropTypes.string,
+	chatData: PropTypes.object,
+	user: PropTypes.object,
+};
 
 export default createNewMessage;
