746a4dca422658d49b39a206345d3ef6c4bc92e9
kevinreber <kevinreber1@gmail.com>
2020-11-02 14:08:39 -0800

    fixed "key" warning

--- src/components/courses/CourseList.jsx
+++ src/components/courses/CourseList.jsx
@@ -1,24 +1,35 @@
 /** Dependencies */
 import React from 'react';
+import { PropTypes } from 'prop-types';
 
 /** Components & Helpers */
 import CourseCard from './CourseCard';
 
 /** Creates a List of User's Courses
  * Courses -> CourseList -> CourseCard -> CourseInfo
+ *
+ * @param {array}	 courses	 Array of objects containing course data.
  */
 function CourseList({ courses = [] }) {
 	const List = courses.map((course) => (
-		<CourseCard
-			id={course.id}
-			department={course.data.course.abbreviation}
-			number={course.data.course.course_number}
-			term={course.data.semester}
-			title={course.data.course.title}
-		/>
+		<>
+			<li key={course.id}>
+				<CourseCard
+					id={course.id}
+					department={course.data.course.abbreviation}
+					number={course.data.course.course_number}
+					term={course.data.semester}
+					title={course.data.course.title}
+				/>
+			</li>
+		</>
 	));
 
-	return <div className='Course-List'>{List}</div>;
+	return <ul className="Course-List">{List}</ul>;
 }
+
+CourseList.propTypes = {
+	courses: PropTypes.array,
+};
 
 export default CourseList;
