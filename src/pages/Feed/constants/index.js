1e0bbf955b0116967dec3dec099700d8ff684edf
kevinreber <kevinreber1@gmail.com>
2020-11-05 11:26:20 -0800

    restructured constants

---
+++ src/pages/Feed/constants/index.js
@@ -0,0 +1,16 @@
+/** Initial State for Image attachments */
+export const INITIAL_STATE_IMAGE = {
+	attachment_preview: '',
+	attachment: '',
+	name: '',
+	url: '',
+};
+
+/** Post Type options */
+export const postTypeOptions = [
+	'Networking',
+	'Campus',
+	'Opportunities',
+	'Marketplace',
+	'Events',
+];
--- src/pages/Feed/constants/postTypeOptions.js
+++
@@ -1,8 +0,0 @@
-/** Post Type options */
-export const postTypeOptions = [
-	'Networking',
-	'Campus',
-	'Opportunities',
-	'Marketplace',
-	'Events',
-];
