1847293b7e709f88a2c69a018e8b1ecc589e4dd2
kevinreber <kevinreber1@gmail.com>
2020-11-05 12:51:54 -0800

    added validator to check file size

--- src/utils/validateImage.js
+++ src/utils/validateImage.js
@@ -1,11 +1,11 @@
 import { PropTypes } from 'prop-types';
 
 /**
- * Function returns boolean value and prompts error message if file is not an image.
+ * Function returns boolean value and prompts error message if file is not an image or file size exceeds 2MB.
  *
  * @param 	{object}     file 			Object of file data.
  * @param 	{function}   setErrors 		Function that returns error messages to error state.
- * @returns {boolean}    Returns boolean checking if file is a validated image.
+ * @returns {boolean}    Returns boolean checking if file is a validated image and file size does not exceed 2MB.
  */
 function fileIsImage(file, setErrors) {
 	if (
@@ -13,6 +13,10 @@ function fileIsImage(file, setErrors) {
 		!file.name.match(/.(jpg|jpeg|png|gif)$/i)
 	) {
 		setErrors('*File not supported');
+		return false;
+	}
+	if (file.size / 1024 / 1024 > 2) {
+		setErrors('*Attachments must be less than 2 MB ');
 		return false;
 	}
 	return true;
