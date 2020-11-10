15f212e4f6dbdb86ce0606f2b4f1603a4cb1f619
kevinreber <kevinreber1@gmail.com>
2020-11-10 10:13:10 -0800

    removed unused code

--- src/store/reducers/assignmentReducer.js
+++ src/store/reducers/assignmentReducer.js
@@ -1,33 +1,4 @@
-const INITIAL_STATE = [
-	{
-		id: 1,
-		title: 'HW 01: Variables & Functions',
-		dueDate: 'Today @4pm',
-		userGrade: null,
-		submitStatus: true,
-	},
-	{
-		id: 2,
-		title: 'HW 02: Data Abstraction',
-		dueDate: 'Tomorrow @7pm',
-		userGrade: null,
-		submitStatus: true,
-	},
-	{
-		id: 3,
-		title: 'Project: Hog',
-		dueDate: 'July 25,2020 @4pm',
-		userGrade: null,
-		submitStatus: true,
-	},
-	{
-		id: 4,
-		title: 'HW 02: High-Order functions',
-		dueDate: 'Aug. 2,2020 @2:30pm',
-		userGrade: null,
-		submitStatus: false,
-	},
-];
+const INITIAL_STATE = [];
 
 const assignmentReducer = (state = INITIAL_STATE, action) => {
 	return state;
--- src/store/reducers/courseReducer.js
+++ src/store/reducers/courseReducer.js
@@ -6,56 +6,6 @@ import {
 	ADD_ASSIGNMENT,
 } from '../actions/types';
 
-// const INITIAL_STATE = [
-// 	{
-// 		id: 'CS61a',
-// 		semester: 'FALL 2020',
-// 		description: 'This is a course',
-// 		assignments: [
-// 			{
-// 				id: 1,
-// 				title: 'HW 01: Variables & Functions',
-// 				dueDate: 'Today @4pm',
-// 				classGrade: 76,
-// 				userGrade: 80,
-// 				submitStatus: true,
-// 				classSubmittals: 10,
-// 			},
-// 			{
-// 				id: 2,
-// 				title: 'HW 02: Data Abstraction',
-// 				dueDate: 'Tomorrow @7pm',
-// 				classGrade: 76,
-// 				userGrade: 80,
-// 				submitStatus: true,
-// 				classSubmittals: 10,
-// 			},
-// 			{
-// 				id: 3,
-// 				title: 'Project: Hog',
-// 				dueDate: 'July 25,2020 @4pm',
-// 				classGrade: 76,
-// 				userGrade: null,
-// 				submitStatus: true,
-// 				classSubmittals: 10,
-// 			},
-// 			{
-// 				id: 4,
-// 				title: 'HW 02: High-Order functions',
-// 				dueDate: 'Aug. 2,2020 @2:30pm',
-// 				classGrade: 76,
-// 				userGrade: 80,
-// 				submitStatus: true,
-// 				classSubmittals: 10,
-// 			},
-// 		],
-// 	},
-// 	{ id: 'CS61b', semester: 'FALL 2020', description: 'This is a course' },
-// 	{ id: 'CS61c', semester: 'FALL 2020', description: 'This is a course' },
-// 	{ id: 'MATH1a', semester: 'FALL 2020', description: 'This is a course' },
-// 	{ id: 'MATH1b', semester: 'FALL 2020', description: 'This is a course' },
-// ];
-
 const courseReducer = (state = [], action) => {
 	switch (action.type) {
 		case FETCH_COURSES:
