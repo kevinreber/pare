276e1c1e13303e6f4d0d70a596b3aff3e64c98b0
kevinreber <kevinreber1@gmail.com>
2020-11-28 21:47:04 -0800

    removed pre-existing text

--- src/pages/Login/components/LoginHeader/LoginHeader.jsx
+++ src/pages/Login/components/LoginHeader/LoginHeader.jsx
@@ -3,10 +3,8 @@ import React from 'react';
 /** Header for Login/Signup */
 function LoginHeader() {
 	return (
-		<div className='Signup-Header'>
-			<h5 className='mate-text-primary font-italic w-50 m-auto'>
-				Stay connected with your College Mates!
-			</h5>
+		<div className="Signup-Header">
+			<h5 className="mate-text-primary font-italic w-50 m-auto"></h5>
 		</div>
 	);
 }
