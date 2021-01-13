61971598e16b92b61aea64e56cdc736b66fc54f2
kevinreber <kevinreber1@gmail.com>
2021-01-12 16:24:39 -0800

    temp: removed berkelytime API call b/c CORS error

--- src/store/actions/courseCatalog.js
+++ src/store/actions/courseCatalog.js
@@ -8,8 +8,9 @@ export function fetchCourseCatalog() {
 	// thunk
 	return async function (dispatch) {
 		try {
-			const response = await axios.get(`${BASE_URL}/api/catalog/catalog_json/`);
-			return dispatch(getCourseCatalog(response.data));
+			// const response = await axios.get(`${BASE_URL}/api/catalog/catalog_json/`);
+			// return dispatch(getCourseCatalog(response.data));
+			return dispatch(getCourseCatalog([]));
 		} catch (err) {
 			dispatch(dispatchError(FETCH_CATALOG_ERROR, err));
 		}
