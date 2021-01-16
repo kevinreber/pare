fa125112ca3af84124a38a3383f4bf50274036ab
kevinreber <kevinreber1@gmail.com>
2021-01-16 14:47:01 -0800

    converted Modal to typescript

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,8 +1,15 @@
 import React, { memo } from 'react';
 import { ReactComponent as CloseIcon } from '../../icons/close-icon.svg';
 import { IconButton } from '@material-ui/core';
-import { PropTypes } from 'prop-types';
+import * as PropTypes from 'prop-types';
 import './Modal.css';
+
+interface ModalProps {
+	isOpen: boolean;
+	content: any;
+	closeModal: Function;
+	full: boolean;
+}
 
 /** Boilerplate template for any actions that require a Modal
  *
@@ -11,13 +18,25 @@ import './Modal.css';
  * @param {function}	closeModal		Function to close/toggle Modal component.
  * @param {boolean}		full			Boolean that toggles style of Modal display.
  */
-function Modal({ isOpen, content, closeModal, full = false }) {
+function Modal({
+	isOpen,
+	content,
+	closeModal,
+	full = false,
+}: ModalProps): JSX.Element {
 	// Modal Height
 	const styles = full ? 'Modal-Full-Content' : 'Modal-Content';
+
+	// NOTE: To Avoid TypeScript error of '`onClick` cannot return `void`',
+	// we have to wrap closeModal in a callback
+	const closeModalDisplay = (): void => {
+		closeModal();
+	};
+
 	return (
 		<div className={`Modal ${isOpen ? 'fade-in' : 'fade-out'}`}>
 			<div className="Modal-Close float-left">
-				<IconButton onClick={closeModal}>
+				<IconButton onClick={closeModalDisplay}>
 					<CloseIcon />
 				</IconButton>
 			</div>
