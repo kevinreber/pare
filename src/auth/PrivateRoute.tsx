5aff2700f7e16d91ed37dc29f392c6100f161eaa
kevinreber <kevinreber1@gmail.com>
2021-01-16 16:12:12 -0800

    installed typescript-react-router and converted PrivateRoute component to TS

--- src/auth/PrivateRoute.tsx
+++ src/auth/PrivateRoute.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
+interface Props {
+	path: string;
+	component: any;
+}
+
 /** Will only allow access to certain routes based on User's credentials */
-function PrivateRoute({ path, component }) {
+function PrivateRoute({ path, component }: Props): JSX.Element {
 	/** User must be logged in, in order to access Private Routes */
-	const currentUser = useSelector((state) => state.auth.user);
+	const currentUser = useSelector((state: any) => state.auth.user);
 
 	/** if user isn't logged in, redirect to login page */
 	if (!currentUser) {
-		return <Redirect to='/login' />;
+		return <Redirect to="/login" />;
 	}
 
 	return <Route exact path={path} component={component} />;
