573c79cd62748527626cbf6cc857c04ffb6da388
kevinreber <kevinreber1@gmail.com>
2021-03-25 10:40:52 -0700

    removed unused code

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,5 @@
 /** Dependencies */
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import { Switch, Redirect } from 'react-router-dom';
 
 /** Component Pages */
@@ -15,77 +15,12 @@ import { StudyGroupChat } from '../pages/StudyGroupChat';
 import { Tutors } from '../pages/Tutors';
 import { UserProfile } from '../pages/UserProfile';
 
-/** Components */
-import Loader from '../components/layout/Loader/Loader';
-
 /** Helpers */
 import PrivateRoute from '../auth/PrivateRoute';
-
-/** Component Pages */
-// const Notifications = lazy(() =>
-// 	import('../pages/Notifications').then((module) => ({
-// 		default: module.Notifications,
-// 	}))
-// );
-
-// const Messages = lazy(() =>
-// 	import('../pages/Messages').then((module) => ({ default: module.Messages }))
-// );
-
-// const Feed = lazy(() =>
-// 	import('../pages/Feed').then((module) => ({ default: module.Feed }))
-// );
-
-// const PostInfo = lazy(() =>
-// 	import('../pages/PostInfo').then((module) => ({ default: module.PostInfo }))
-// );
-
-// const Search = lazy(() =>
-// 	import('../pages/Search').then((module) => ({ default: module.Search }))
-// );
-
-// const Courses = lazy(() =>
-// 	import('../pages/Courses').then((module) => ({ default: module.Courses }))
-// );
-
-// const CourseInfo = lazy(() =>
-// 	import('../pages/CourseInfo').then((module) => ({
-// 		default: module.CourseInfo,
-// 	}))
-// );
-
-// const StudyGroups = lazy(() =>
-// 	import('../pages/StudyGroups').then((module) => ({
-// 		default: module.StudyGroups,
-// 	}))
-// );
-
-// const StudyGroupChat = lazy(() =>
-// 	import('../pages/StudyGroupChat').then((module) => ({
-// 		default: module.StudyGroupChat,
-// 	}))
-// );
-
-// const Tutors = lazy(() =>
-// 	import('../pages/Tutors').then((module) => ({ default: module.Tutors }))
-// );
-
-// const UserProfile = lazy(() =>
-// 	import('../pages/UserProfile').then((module) => ({
-// 		default: module.UserProfile,
-// 	}))
-// );
-
-// const PrivateRoute = lazy(() =>
-// 	import('../auth/PrivateRoute').then((module) => ({
-// 		default: module.PrivateRoute,
-// 	}))
-// );
 
 function Routes() {
 	return (
 		<>
-			{/* <Suspense fallback={<Loader />}> */}
 			<Switch>
 				<PrivateRoute exact path="/feed" component={Feed} />
 				<PrivateRoute exact path="/messages" component={Notifications} />
@@ -104,7 +39,6 @@ function Routes() {
 				<PrivateRoute exact path="/users/:userId" component={UserProfile} />
 				<Redirect to="/feed" />
 			</Switch>
-			{/* </Suspense> */}
 		</>
 	);
 }
